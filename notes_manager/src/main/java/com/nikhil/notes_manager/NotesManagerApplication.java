30165c60d4005f4218eac29a84e7c8b0e5723b3f
nikhilpounikar <nikhilpounikar@gmail.com>
2024-01-04 18:21:51 +0530

    added files

---
+++ notes_manager/src/main/java/com/nikhil/notes_manager/NotesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.notes_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesManagerApplication.class, args);
+	}
+
+}
---
+++ notes_manager/src/test/java/com/nikhil/notes_manager/NotesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.notes_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
